8a248da8373c31c88d8966d7f5bae55d039389f3
Darrell Wright <dwright-git@dawdevel.ca>
2017-09-30 13:59:10 -0400

    Migrated function_stream_test to use Boost Test

--- tests/function_stream_test.cpp
+++ tests/function_stream_test.cpp
@@ -25,6 +25,8 @@
 #include <random>
 #include <string>
 
+#define BOOST_TEST_MODULE function_stream
+#include <daw/boost_test.h>
 #include <daw/daw_benchmark.h>
 #include <daw/daw_size_literals.h>
 
@@ -32,74 +34,67 @@
 
 using namespace daw::size_literals;
 
-// using real_t = boost::multiprecision::number<boost::multiprecision::cpp_dec_float<12500>>;
-using real_t = long double;
-
-real_t operator"" _R( long double d ) {
-	return real_t{d};
-}
-
-real_t fib( real_t n ) noexcept {
-	real_t last = 0;
-	real_t result = 1;
-	for( uintmax_t m = 1; m < n; ++m ) {
-		auto new_last = result;
-		result += result + last;
-		last = new_last;
+namespace part1 {
+	double operator"" _R( long double d ) {
+		return static_cast<double>( d );
 	}
-	return result;
-}
 
-int a( int x ) {
-	return x * 2;
-}
-int b( int x ) {
-	return x * 3;
-}
-int c( int x ) {
-	return x * 4;
-}
-
-struct A {
-	int operator( )( int x ) const {
-		return 1;
+	double fib( double n ) noexcept {
+		double last = 0;
+		double result = 1;
+		for( uintmax_t m = 1; m < n; ++m ) {
+			auto new_last = result;
+			result += result + last;
+			last = new_last;
+		}
+		return result;
 	}
-};
 
-struct B {
-	int operator( )( int x ) const {
-		return 2;
+	int a( int x ) {
+		return x * 2;
 	}
-};
-
-struct C {
-	void operator( )( std::string const &x ) const {}
-};
-
-struct D {
-	std::string operator( )( int x ) const {
-		return std::string{"Hello"};
+	int b( int x ) {
+		return x * 3;
 	}
-};
+	int c( int x ) {
+		return x * 4;
+	}
 
-std::string blah( int i ) {
-	return std::to_string( i );
-}
+	struct A {
+		int operator( )( int x ) const {
+			return 1;
+		}
+	};
 
-int main( int argc, char **argv ) {
-	if( false ) {
+	struct B {
+		int operator( )( int x ) const {
+			return 2;
+		}
+	};
+
+	struct C {
+		void operator( )( std::string const &x ) const {}
+	};
+
+	struct D {
+		std::string operator( )( int x ) const {
+			return std::string{"Hello"};
+		}
+	};
+
+	BOOST_AUTO_TEST_CASE( function_composer_test ) {
 		daw::impl::function_composer_t<A, B, D> fc{A{}, B{}, D{}};
 		static_assert( std::is_same<decltype( fc.apply( 3 ) ), decltype( D{}( 3 ) )>::value,
 		               "function_composer_t is not returning the correct type" );
 		std::cout << fc.apply( 4 ) << std::endl;
 	}
 
-	if( false ) {
+	BOOST_AUTO_TEST_CASE( function_stream_test_001 ) {
 		auto fs = daw::make_function_stream( &a, &b, &c );
 		std::cout << fs( 1 ).get( ) << std::endl;
 	}
 
-	if( false ) {
+	BOOST_AUTO_TEST_CASE( function_stream_test_002 ) {
 		std::random_device rd;
 		std::mt19937 gen( rd( ) );
 		std::uniform_int_distribution<> dis( 5, 7 );
@@ -123,113 +118,118 @@ int main( int argc, char **argv ) {
 		std::cout << *std::get<0>( f_grp ) << '\n';
 		std::cout << *std::get<1>( f_grp ) << '\n';
 	}
-	if( false ) {
-		auto ts = daw::get_task_scheduler( );
-		auto t = daw::make_future_result( []( ) {
-			         std::cout << "part1\n";
-			         std::cout << std::endl;
-			         return 2;
-		         } )
-		             .next( []( int i ) {
-			             std::cout << "part" << i << '\n';
-			             std::cout << "hahaha\n";
-			             std::cout << std::endl;
-		             } );
+} // namespace fc_test
 
-		t.wait( );
+std::string blah( int i ) {
+	return std::to_string( i );
+}
 
-		std::cout << "operator>>\n";
+BOOST_AUTO_TEST_CASE( future_result_test_001 ) {
+	auto ts = daw::get_task_scheduler( );
+	auto t = daw::make_future_result( []( ) {
+		         std::cout << "part1\n";
+		         std::cout << std::endl;
+		         return 2;
+	         } )
+	             .next( []( int i ) {
+		             std::cout << "part" << i << '\n';
+		             std::cout << "hahaha\n";
+		             std::cout << std::endl;
+	             } );
 
-		auto u = daw::make_future_result( []( ) {
-			std::cout << "part1\n";
-			std::cout << std::endl;
-			return 2;
-		} );
+	t.wait( );
 
-		auto result = u >> []( int i ) {
-			std::cout << "part" << i << '\n';
-			std::cout << "hahaha\n";
-			std::cout << std::endl;
-			return i + 1;
-		} >> blah >> []( std::string s ) { std::cout << s << "\nfin\n"; };
+	std::cout << "operator>>\n";
 
-		auto v = daw::make_future_generator( []( ) {
-			std::cout << "part1\n";
-			std::cout << std::endl;
-			return 2;
-		} );
+	auto u = daw::make_future_result( []( ) {
+		std::cout << "part1\n";
+		std::cout << std::endl;
+		return 2;
+	} );
 
-		auto result2 = v >> []( int i ) {
-			std::cout << "part" << i << '\n';
-			std::cout << "hahaha\n";
-			std::cout << std::endl;
-			return i + 1;
-		} >> blah >> []( std::string s ) { std::cout << s << "\nfin\n"; };
+	auto result = u >> []( int i ) {
+		std::cout << "part" << i << '\n';
+		std::cout << "hahaha\n";
+		std::cout << std::endl;
+		return i + 1;
+	} >> blah >> []( std::string s ) { std::cout << s << "\nfin\n"; };
 
-		result2( ).wait( );
-	}
+	auto v = daw::make_future_generator( []( ) {
+		std::cout << "part1\n";
+		std::cout << std::endl;
+		return 2;
+	} );
 
-	{
-		auto make_values = []( size_t howmany ) {
-			std::vector<int> vs( howmany );
-			std::generate( vs.begin( ), vs.end( ), std::rand );
-			return vs;
-		};
+	auto result2 = v >> []( int i ) {
+		std::cout << "part" << i << '\n';
+		std::cout << "hahaha\n";
+		std::cout << std::endl;
+		return i + 1;
+	} >> blah >> []( std::string s ) { std::cout << s << "\nfin\n"; };
 
-		auto sort_values = []( auto values ) {
-			std::sort( values.begin( ), values.end( ) );
-			return std::move( values );
-		};
+	result2( ).wait( );
+}
 
-		auto show_values = []( auto values ) {
-			std::cout << '\n';
-			for( auto const &value : values ) {
-				std::cout << " " << value;
-			}
-			std::cout << '\n';
-			return std::move( values );
-		};
+BOOST_AUTO_TEST_CASE( composable_function_stream_test_001 ) {
+	auto make_values = []( size_t howmany ) {
+		std::vector<int> vs( howmany );
+		std::generate( vs.begin( ), vs.end( ), std::rand );
+		return vs;
+	};
 
-		auto odd_values = []( auto values ) {
-			values.erase( std::remove_if( values.begin( ), values.end( ), []( auto const &i ) { return i % 2 == 0; } ),
-			              values.end( ) );
-			return std::move( values );
-		};
+	auto sort_values = []( auto values ) {
+		std::sort( values.begin( ), values.end( ) );
+		return std::move( values );
+	};
 
-		auto sum_values = []( auto values ) {
-			intmax_t r = 0;
-			for( auto const &v : values ) {
-				r += v;
-			}
-			return r;
-		};
+	/*
+	auto show_values = []( auto values ) {
+		std::cout << '\n';
+		for( auto const &value : values ) {
+			std::cout << " " << value;
+		}
+		std::cout << '\n';
+		return std::move( values );
+	};*/
 
-		auto show_value = []( auto value ) {
-			std::cout << value << '\n';
-			return std::move( value );
-		};
+	auto odd_values = []( auto values ) {
+		values.erase( std::remove_if( values.begin( ), values.end( ), []( auto const &i ) { return i % 2 == 0; } ),
+		              values.end( ) );
+		return std::move( values );
+	};
 
-		auto const values = make_values( 75_MB );
+	auto sum_values = []( auto values ) {
+		intmax_t r = 0;
+		for( auto const &v : values ) {
+			r += v;
+		}
+		return r;
+	};
 
-		auto fsp2 = daw::make_future_generator( sort_values ) >> odd_values >> sum_values >> show_value;
-		daw::get_task_scheduler( ).start( );
+	auto show_value = []( auto value ) {
+		std::cout << value << '\n';
+		return std::move( value );
+	};
 
-		auto t1 = daw::benchmark(
-		    [&]( ) { wait_for_function_streams( fsp2( values ), fsp2( values ), fsp2( values ), fsp2( values ) ); } );
+	auto const values = make_values( 75_MB );
 
-		std::cout << "Parallel stream time " << daw::utility::format_seconds( t1, 2 ) << '\n';
+	auto fsp2 = daw::make_future_generator( sort_values ) >> odd_values >> sum_values >> show_value;
+	daw::get_task_scheduler( ).start( );
 
-		auto t2 = daw::benchmark( [&]( ) {
-			auto composed = [&]( auto s ) { return show_value( sum_values( odd_values( sort_values( s ) ) ) ); };
-			composed( values );
-			composed( values );
-			composed( values );
-			composed( values );
-		} );
+	auto t1 = daw::benchmark(
+	    [&]( ) { wait_for_function_streams( fsp2( values ), fsp2( values ), fsp2( values ), fsp2( values ) ); } );
 
-		std::cout << "Sequential time " << daw::utility::format_seconds( t2, 2 ) << '\n';
-		std::cout << "Diff " << (t2/t1) << '\n';
-	}	
-	return EXIT_SUCCESS;
+	std::cout << "Parallel stream time " << daw::utility::format_seconds( t1, 2 ) << '\n';
+
+	auto t2 = daw::benchmark( [&]( ) {
+		auto composed = [&]( auto s ) { return show_value( sum_values( odd_values( sort_values( s ) ) ) ); };
+		composed( values );
+		composed( values );
+		composed( values );
+		composed( values );
+	} );
+
+	std::cout << "Sequential time " << daw::utility::format_seconds( t2, 2 ) << '\n';
+	std::cout << "Diff " << ( t2 / t1 ) << '\n';
 }
 
