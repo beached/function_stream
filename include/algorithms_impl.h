0cdbed21f56b1f58559fcca251e592fdfc9c2e9b
Darrell Wright <dwright-git@dawdevel.ca>
2017-12-20 12:16:40 -0500

    .

--- include/algorithms_impl.h
+++ include/algorithms_impl.h
@@ -150,12 +150,11 @@ namespace daw {
 					daw::shared_semaphore sem{1 - static_cast<intmax_t>( ranges.size( ) )};
 					partition_range( ranges,
 					                 [func, first]( auto f, auto l ) {
-						                 auto const start_pos = static_cast<size_t>( first, f );
-						                 auto const end_pos = static_cast<size_t>( first, l );
+						                 auto const start_pos = static_cast<size_t>( std::distance( first, f ) );
+						                 auto const end_pos = static_cast<size_t>( std::distance( first, l ) );
 						                 for( size_t n = start_pos; n < end_pos; ++n ) {
 							                 func( n );
 						                 }
-
 					                 },
 					                 ts )
 					  .wait( );
