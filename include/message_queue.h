fbfc3362bbcb9453b9303f9e2a318b58b03afbd3
Darrell Wright <dwright-git@dawdevel.ca>
2018-05-17 23:27:08 -0400

    .

--- include/message_queue.h
+++ include/message_queue.h
@@ -111,7 +111,6 @@ namespace daw {
 			using reference = value_t &;
 			using const_reference = value_t const &;
 
-		private:
 			template<typename... Args,
 			         std::enable_if_t<(!daw::traits::is_type_v<msg_ptr_t, Args...> &&
 			                           !daw::traits::is_type_v<pointer, Args...>),
